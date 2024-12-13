5930b19b621b9e3126b735b206c6583188890ed6
Hinarafiq <161075228+Hinarafiq@users.noreply.github.com>
2024-12-13 15:15:00 +0500

    Update Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 export default function Hero(){
     return (
         <div className="w-full h-[900px]  bg-[#FBEBB5] grid md:flex">
-            <div className="w-[440px] h-[192px] pt-64 md:pt-[345px] ml-4 md:ml-[342px] leading-[96px]">
+            <div className="w-[340px] h-[192px] pt-64 md:pt-[345px] ml-4 md:ml-[342px] leading-[96px]">
                <h1 className="font-bold text-2xl md:text-6xl">Rocket single seater</h1>
                <h1 className="w-[121px] h-[49px] mt-6 text-xl md:text-2xl"><u>Shop Now</u></h1>
             </div>
