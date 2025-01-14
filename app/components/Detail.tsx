75abdbf7c3bf1162facb6bbf14be9864eadf32c8
Hinarafiq <161075228+Hinarafiq@users.noreply.github.com>
2025-01-14 14:10:23 +0500

    Update Detail.tsx

--- app/components/Detail.tsx
+++ app/components/Detail.tsx
@@ -1,7 +1,7 @@
 export default function Detail () {
     return (
        <div className="bg-[url(/BgPic.png)] w-full h-[450px] mt-8 bg-cover bg-no-repeat">
-           <div className="md:w-[454px] h-[202px] md:ml-[693px] pt-[150px] text-center">
+           <div className="md:w-[454px] h-[202px] md:ml-[393px] pt-[150px] text-center">
              <ul>
                 <li className="font-bold text-6xl">Our Instagram</li>
                 <li className="mt-6 text-2xl font-sans">Follow our store on Instagram</li>
